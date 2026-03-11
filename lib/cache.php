636ba6c1c5505aea666431a5833f52f024b98071
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 16:02:34 +0100

    Update cache.php

--- lib/cache.php
+++ lib/cache.php
@@ -4,20 +4,11 @@ if (!defined("WHMCS")) {
     die("Access denied");
 }
 
+function setup_cache($zone,$token){
+    cf_api("zones/$zone/settings/cache_level","PATCH",["value"=>"standard"], $token);
+    cf_api("zones/$zone/settings/brotli","PATCH",["value"=>"on"], $token);
+}
+
 function purge_cache($zone,$token){
-
-cf_api(
-
-"zones/$zone/purge_cache",
-
-"POST",
-
-[
-"purge_everything"=>true
-],
-
-$token
-
-);
-
+    cf_api("zones/$zone/purge_cache","POST",["purge_everything"=>true], $token);
 }
