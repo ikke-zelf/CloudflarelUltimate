7f047e8f8306ceb8b18f3f9403aae26acfdd5bc5
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 16:37:52 +0100

    Update analytics.php

--- lib/analytics.php
+++ lib/analytics.php
@@ -1,21 +1,11 @@
 <?php
+if(!defined("WHMCS")) die("Access denied");
 
-if (!defined("WHMCS")) {
-    die("Access denied");
-}
-
-function get_cf_analytics($zone,$token){
-
-return cf_api(
-
-"zones/$zone/analytics/dashboard",
-
-"GET",
-
-[],
-
-$token
-
-);
-
+function get_cf_analytics($zone, $token){
+    return cf_api(
+        "zones/$zone/analytics/dashboard",
+        "GET",
+        [],
+        $token
+    );
 }
