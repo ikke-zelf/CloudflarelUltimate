227ca66d994d2e1048835fe758b1abe7dc5ce839
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 16:04:02 +0100

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -13,43 +13,28 @@ require_once __DIR__.'/lib/cache.php';
 require_once __DIR__.'/lib/firewall.php';
 
 add_hook('AfterModuleCreate',1,function($params){
+    $domain = $params['domain'];
+    $ip = $params['serverip'];
 
-$domain=$params['domain'];
-$ip=$params['serverip'];
+    $settings = Capsule::table('tbladdonmodules')
+        ->where('module','cloudflareultimate')
+        ->pluck('value','setting');
 
-$settings=Capsule::table('tbladdonmodules')
-->where('module','cloudflareultimate')
-->pluck('value','setting');
+    $token = $settings['apitoken'];
+    $account = $settings['accountid'];
 
-$token=$settings['apitoken'];
-$account=$settings['accountid'];
+    $zone = cf_api("zones","POST",[
+        "name"=>$domain,
+        "account"=>["id"=>$account],
+        "jump_start"=>true
+    ], $token);
 
-$zone=cf_api(
+    if(!$zone['success']) return;
 
-"zones",
-"POST",
-[
-"name"=>$domain,
-"account"=>["id"=>$account],
-"jump_start"=>true
-],
+    $zoneid = $zone['result']['id'];
 
-$token
-
-);
-
-if(!$zone['success']){
-return;
-}
-
-$zoneid=$zone['result']['id'];
-
-create_dns($zoneid,$domain,$ip,$token);
-
-setup_ssl($zoneid,$token);
-
-setup_cache($zoneid,$token);
-
-setup_firewall($zoneid,$token);
-
+    create_dns($zoneid,$domain,$ip,$token);
+    setup_ssl($zoneid,$token);
+    setup_cache($zoneid,$token);
+    setup_firewall($zoneid,$token);
 });
