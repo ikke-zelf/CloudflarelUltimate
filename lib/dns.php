69f940ddb2160ca131a6bbeca307eacfb06591fb
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 16:03:24 +0100

    Update dns.php

--- lib/dns.php
+++ lib/dns.php
@@ -5,46 +5,23 @@ if (!defined("WHMCS")) {
 }
 
 function create_dns($zone,$domain,$ip,$token){
+    cf_api("zones/$zone/dns_records","POST",[
+        "type"=>"A",
+        "name"=>$domain,
+        "content"=>$ip,
+        "ttl"=>1,
+        "proxied"=>true
+    ], $token);
 
-cf_api(
-
-"zones/$zone/dns_records",
-"POST",
-[
-"type"=>"A",
-"name"=>$domain,
-"content"=>$ip,
-"ttl"=>1,
-"proxied"=>true
-],
-$token
-);
-
-cf_api(
-
-"zones/$zone/dns_records",
-"POST",
-[
-"type"=>"CNAME",
-"name"=>"www",
-"content"=>$domain,
-"ttl"=>1,
-"proxied"=>true
-],
-$token
-);
-
+    cf_api("zones/$zone/dns_records","POST",[
+        "type"=>"CNAME",
+        "name"=>"www",
+        "content"=>$domain,
+        "ttl"=>1,
+        "proxied"=>true
+    ], $token);
 }
 
 function get_dns_records($zone,$token){
-
-return cf_api(
-
-"zones/$zone/dns_records",
-"GET",
-[],
-$token
-
-);
-
+    return cf_api("zones/$zone/dns_records","GET",[], $token);
 }
