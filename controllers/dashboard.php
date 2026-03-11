470ba5a636f79ee4b4c99c9f3621d8f64d01158b
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 15:53:58 +0100

    Update dashboard.php

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,4 +1,11 @@
-$stats=get_cf_analytics($zoneid,$token);
+<?php
 
-$smarty->assign("requests",$stats['result']['totals']['requests']['all']);
-$smarty->assign("bandwidth",$stats['result']['totals']['bandwidth']['all']);
+require_once __DIR__ . '/../lib/analytics.php';
+
+$stats = get_cf_analytics($zoneid, $token);
+
+$requests = $stats['result']['totals']['requests']['all'] ?? 0;
+$bandwidth = $stats['result']['totals']['bandwidth']['all'] ?? 0;
+
+$smarty->assign("requests", $requests);
+$smarty->assign("bandwidth", $bandwidth);
