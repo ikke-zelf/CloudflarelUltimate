fc9921130bd745975b9fee20d52786323ac82124
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 16:35:25 +0100

    Update dashboard.php

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,15 +1,8 @@
 <?php
+if(!defined("WHMCS")) die("Access denied");
+require_once __DIR__.'/../lib/analytics.php';
 
-if (!defined("WHMCS")) {
-    die("Access denied");
-}
+$stats = get_cf_analytics($zoneid,$token);
 
-require_once __DIR__ . '/../lib/analytics.php';
-
-$stats = get_cf_analytics($zoneid, $token);
-
-$requests = $stats['result']['totals']['requests']['all'] ?? 0;
-$bandwidth = $stats['result']['totals']['bandwidth']['all'] ?? 0;
-
-$smarty->assign("requests", $requests);
-$smarty->assign("bandwidth", $bandwidth);
+$smarty->assign("requests",$stats['result']['totals']['requests']['all'] ?? 0);
+$smarty->assign("bandwidth",$stats['result']['totals']['bandwidth']['all'] ?? 0);
