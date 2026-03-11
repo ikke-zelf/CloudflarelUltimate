a26c5f2b720027b5ba1c34adf82948f8845efdce
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 12:32:33 +0100

    Create firewall.php
    Firewall rules

---
+++ lib/firewall.php
@@ -0,0 +1,24 @@
+function setup_firewall($zone,$token){
+
+cf_api(
+
+"zones/$zone/firewall/rules",
+
+"POST",
+
+[
+"action"=>"block",
+
+"filter"=>[
+"expression"=>"(http.request.uri.path contains \"wp-login.php\")"
+],
+
+"description"=>"WordPress brute force protection"
+
+],
+
+$token
+
+);
+
+}
