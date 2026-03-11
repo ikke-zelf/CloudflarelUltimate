b8a39eed53487a3a774046ff09dec1df0fd78279
ikke-zelf <65031378+ikke-zelf@users.noreply.github.com>
2026-03-11 16:02:13 +0100

    Update ssl.php

--- lib/ssl.php
+++ lib/ssl.php
@@ -4,11 +4,6 @@ if (!defined("WHMCS")) {
     die("Access denied");
 }
 
-function setup_cache($zone,$token){
-    cf_api("zones/$zone/settings/cache_level","PATCH",["value"=>"standard"], $token);
-    cf_api("zones/$zone/settings/brotli","PATCH",["value"=>"on"], $token);
-}
-
-function purge_cache($zone,$token){
-    cf_api("zones/$zone/purge_cache","POST",["purge_everything"=>true], $token);
+function setup_ssl($zone,$token){
+    cf_api("zones/$zone/settings/ssl","PATCH",["value"=>"full"], $token);
 }
